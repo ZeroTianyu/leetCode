7d954ff2183faeee1f4396b08e362a25e01257d5
GuoTy <1846548114@qq.com>
2020-04-28 11:46:51 +0800

    update

--- src/com/marmot/leetcode/solution697/Demo1.java
+++ src/com/marmot/leetcode/solution697/Demo1.java
@@ -1,6 +1,7 @@
 package com.marmot.leetcode.solution697;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,9 +25,25 @@ public class Demo1 {
     }
 
     public int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> left = new HashMap(),
+                right = new HashMap(), count = new HashMap();
 
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if (left.get(x) == null) {
+                left.put(x, i);
+            }
+            right.put(x, i);
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
 
-
-        return 0;
+        int ans = nums.length;
+        int degree = Collections.max(count.values());
+        for (int x : count.keySet()) {
+            if (count.get(x) == degree) {
+                ans = Math.min(ans, right.get(x) - left.get(x) + 1);
+            }
+        }
+        return ans;
     }
 }
