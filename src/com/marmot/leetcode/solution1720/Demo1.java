9593f91a20d53f7a2e5ecada00145a4ff6835c3a
郭天宇 <1846548114@qq.com>
2021-05-06 11:17:21 +0800

    feat(137): 异或运算 可看作是不做进位的加法运算

---
+++ src/com/marmot/leetcode/solution1720/Demo1.java
@@ -0,0 +1,37 @@
+package com.marmot.leetcode.solution1720;
+
+/**
+ * 未知 整数数组 arr 由 n 个非负整数组成。
+ * <p>
+ * 经编码后变为长度为 n - 1 的另一个整数数组 encoded ，其中 encoded[i] = arr[i] XOR arr[i + 1] 。例如，arr = [1,0,2,1] 经编码后得到 encoded = [1,2,3] 。
+ * <p>
+ * 给你编码后的数组 encoded 和原数组 arr 的第一个元素 first（arr[0]）。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/decode-xored-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author: guotianyu
+ * @description:
+ * @create: 2021/05/06 11:08
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        Demo1 demo1 = new Demo1();
+        int[] encoded = new int[]{1, 2, 3};
+
+
+        demo1.decode(encoded, 1);
+    }
+
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length + 1;
+        int[] arr = new int[n];
+        arr[0] = first;
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] ^ encoded[i - 1];
+        }
+        return arr;
+    }
+}
