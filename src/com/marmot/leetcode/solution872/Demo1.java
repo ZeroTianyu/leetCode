b3db28f1784684a87ebdc6205ed2c2001240cfad
郭天宇 <1846548114@qq.com>
2021-05-10 11:01:19 +0800

    feat(872): 叶子相似的树

--- src/com/marmot/leetcode/solution872/Demo1.java
+++ src/com/marmot/leetcode/solution872/Demo1.java
@@ -7,7 +7,52 @@ import java.util.List;
 
 /**
  * @author: guotianyu
- * @description:
+ * @description: 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ * <p>
+ * <p>
+ * <p>
+ * 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。
+ * <p>
+ * 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+ * <p>
+ * 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root1 = [1], root2 = [1]
+ * 输出：true
+ * 示例 3：
+ * <p>
+ * 输入：root1 = [1], root2 = [2]
+ * 输出：false
+ * 示例 4：
+ * <p>
+ * 输入：root1 = [1,2], root2 = [2,2]
+ * 输出：true
+ * 示例 5：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root1 = [1,2,3], root2 = [1,3,2]
+ * 输出：false
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 给定的两棵树可能会有 1 到 200 个结点。
+ * 给定的两棵树上的值介于 0 到 200 之间。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/leaf-similar-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @create: 2021/05/10 09:49
  */
 public class Demo1 {
