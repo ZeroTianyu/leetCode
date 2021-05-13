329eb98e08ecda145ba45fd2f4dde54243c63ab1
郭天宇 <1846548114@qq.com>
2021-05-13 10:35:07 +0800

    feat(1310): 数组异或  增加官方解

--- src/com/marmot/leetcode/solution1310/Demo1.java
+++ src/com/marmot/leetcode/solution1310/Demo1.java
@@ -55,7 +55,13 @@ public class Demo1 {
         }
     }
 
-
+    /**
+     * 个人解
+     *
+     * @param arr
+     * @param queries
+     * @return
+     */
     public int[] xorQueries(int[] arr, int[][] queries) {
 
         int[] nums = new int[queries.length];
@@ -73,5 +79,27 @@ public class Demo1 {
             nums[q] = xorNum;
         }
         return nums;
+    }
+
+
+    /**
+     * 官方解
+     *
+     * @param arr
+     * @param queries
+     * @return
+     */
+    public int[] xorQueries1(int[] arr, int[][] queries) {
+        int n = arr.length;
+        int[] xors = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            xors[i + 1] = xors[i] ^ arr[i];
+        }
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            ans[i] = xors[queries[i][0]] ^ xors[queries[i][1] + 1];
+        }
+        return ans;
     }
 }
