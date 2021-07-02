fd39d1bbab3be34817c5dc462e3970e4fdfc27dd
郭天宇 <1846548114@qq.com>
2021-07-02 15:55:34 +0800

    feat(1833): 雪糕的最大数量

---
+++ src/com/marmot/interview/Interview38.java
@@ -0,0 +1,26 @@
+package com.marmot.interview;
+
+/**
+ * @author: guotianyu
+ * @description:输入一个字符串，打印出该字符串中字符的所有排列。  
+ * <p>
+ * 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入：s = "abc"
+ * 输出：["abc","acb","bac","bca","cab","cba"]
+ * <p>
+ * @create: 2021/06/22 09:18
+ */
+public class Interview38 {
+
+    public String[] permutation(String s) {
+        String[] str = new String[]{};
+
+
+        return null;
+    }
+}
