07b539c86a3e1b328722141ea29d054d5eefc538
郭天宇 <1846548114@qq.com>
2021-07-22 13:51:38 +0800

    feat(724):  寻找数组的中心下标

---
+++ src/com/marmot/leetcode/solution724/Demo1.java
@@ -0,0 +1,65 @@
+package com.marmot.leetcode.solution724;
+
+import java.util.Arrays;
+
+/**
+ * @author: guotianyu
+ * @description: 给你一个整数数组 nums ，请计算数组的 中心下标 。
+ * <p>
+ * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+ * <p>
+ * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+ * <p>
+ * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1, 7, 3, 6, 5, 6]
+ * 输出：3
+ * 解释：
+ * 中心下标是 3 。
+ * 左侧数之和 sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11 ，
+ * 右侧数之和 sum = nums[4] + nums[5] = 5 + 6 = 11 ，二者相等。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1, 2, 3]
+ * 输出：-1
+ * 解释：
+ * 数组中不存在满足此条件的中心下标。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [2, 1, -1]
+ * 输出：0
+ * 解释：
+ * 中心下标是 0 。
+ * 左侧数之和 sum = 0 ，（下标 0 左侧不存在元素），
+ * 右侧数之和 sum = nums[1] + nums[2] = 1 + -1 = 0 。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 104
+ * -1000 <= nums[i] <= 1000
+ * @create: 2021/07/22 11:23
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        Demo1 demo1 = new Demo1();
+        int[] nums = new int[]{-1, -1, 0, 1, 1, 0};
+        int i = demo1.pivotIndex(nums);
+        System.out.println(i);
+    }
+
+    public int pivotIndex(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+}
