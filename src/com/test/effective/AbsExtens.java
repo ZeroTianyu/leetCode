876b89e19998244595970ec9b81b360cfd0f01be
郭天宇 <1846548114@qq.com>
2021-10-08 18:09:10 +0800

    dsa

--- src/com/test/effective/AbsExtens.java
+++ src/com/test/effective/AbsExtens.java
@@ -6,6 +6,7 @@ package com.test.effective;
  * @Create 2020-04-17 18:08
  **/
 public class AbsExtens extends Abs {
+    
     @Override
     public String handler(double d) {
         return "hello" + d;
